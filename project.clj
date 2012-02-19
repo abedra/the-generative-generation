23f32a81eb62f84311d70ce7fe7c1cbf2c14edc5
Aaron Bedra <aaron@aaronbedra.com>
2012-02-19 15:05:26 -0600

    initial commit

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject clojurebreaker "0.0.1"
+  :description "Clojure/West tutorial"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/math.combinatorics "0.0.1"]
+                 [org.clojure/test.generative "0.1.3"]])
